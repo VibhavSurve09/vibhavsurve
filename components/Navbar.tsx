714eb227545a4b5540b10b344bf77584440d72f4
Vibhav0 <vibhav.surve@xaviers.edu.in>
2022-01-22 16:01:44 +0530

    Working with Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,56 +1,63 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
-const Navbar = () => {
-  const [activePage, setActivePage] = useState<string>('About');
-  const router = useRouter();
-  useEffect(() => {
-    const { pathname } = router;
-    console.log(pathname);
-  }, []);
+import { FunctionComponent, useEffect, useState } from 'react';
+const NavIteam: FunctionComponent<{
+  activePage: string;
+  setActivePage: Function;
+  name: string;
+  route: string;
+}> = ({ activePage, setActivePage, name, route }) => {
   return (
-    <div>
-      <span className='font-bold text-green'>{activePage}</span>
-      <div className='flex space-x-3'>
-        {activePage !== 'About' && (
-          <Link href='/'>
-            <a>
-              <span
-                onClick={() => {
-                  setActivePage('About');
-                }}
-              >
-                About
-              </span>
-            </a>
-          </Link>
-        )}
-        {activePage !== 'Resume' && (
-          <Link href='/resume'>
-            <a>
-              <span
-                onClick={() => {
-                  setActivePage('Resume');
-                }}
-              >
-                Resume
-              </span>
-            </a>
-          </Link>
-        )}
-        {activePage !== 'Projects' && (
-          <Link href='/projects'>
-            <a>
-              <span
-                onClick={() => {
-                  setActivePage('Projects');
-                }}
-              >
-                Projects
-              </span>
-            </a>
-          </Link>
-        )}
+    activePage !== name && (
+      <Link href={route}>
+        <a>
+          <span
+            className='hover:text-green'
+            onClick={() => {
+              setActivePage(name);
+            }}
+          >
+            {name}
+          </span>
+        </a>
+      </Link>
+    )
+  );
+};
+const Navbar = () => {
+  const [activePage, setActivePage] = useState<string>('');
+  const router = useRouter();
+  const { pathname } = router;
+  useEffect(() => {
+    if (pathname === '/') setActivePage('About');
+    if (pathname === '/resume') setActivePage('Resume');
+    if (pathname === '/projects') setActivePage('Projects');
+  }, [pathname]);
+  return (
+    <div className='flex justify-between px-5 py-3 my-3'>
+      <span className='text-xl font-bold border-b-4 text-green border-green md:text-2xl'>
+        {activePage}
+      </span>
+      <div className='flex space-x-5 text-lg'>
+        <NavIteam
+          activePage={activePage}
+          setActivePage={setActivePage}
+          name='About'
+          route='/'
+        />
+
+        <NavIteam
+          activePage={activePage}
+          setActivePage={setActivePage}
+          name='Resume'
+          route='/resume'
+        />
+        <NavIteam
+          activePage={activePage}
+          setActivePage={setActivePage}
+          name='Projects'
+          route='/projects'
+        />
       </div>
     </div>
   );
